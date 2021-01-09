e01e38458b7447fd34c7bffb1a94001e09044d6a
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2021-01-09 14:39:16 -0800

    initial commit

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 options = Options()
-options.add_argument("user-data-dir=C:/Users/new22/AppData/Local/Google/Chrome/User Data")
+options.add_argument("user-data-dir=C:/Users/your user name/AppData/Local/Google/Chrome/User Data") #change user name here 
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.walmart.ca/en/ip/t157-tripod/6000200406124")
 
 
-# # while(driver.find_elements_by_xpath("//*[text() = 'Add to Cart']").size() == 0 ):
-# #     driver.refresh()
+while(driver.find_elements_by_xpath("//*[text() = 'Add to Cart']").size() == 0 ):
+    driver.refresh()
 wait = WebDriverWait(driver,10)
 element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'css-1i45fk4')))
 add = driver.find_element_by_class_name('css-1i45fk4')
--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ from cookies import save_cookie
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 
-driver = webdriver.Firefox()
-driver.get("https://www.walmart.ca/en/ip/t157-tripod/6000200406124")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.walmart.ca/en/ip/t157-tripod/6000200406124") #insert the website you want to track here 
 
 #navigate to sign-in page
 elem = driver.find_element_by_class_name("css-1xh2uh0").click()
 
-#fill in the form, fill in your account info here
+#fill in the form, fill in your account info here to login
 email = driver.find_element_by_id("username")
-email.send_keys("new2282307660@gmail.com")  
+email.send_keys("<insert your email here>")  
 password = driver.find_element_by_id("password")
-password.send_keys("@$smolFitness!") 
+password.send_keys("<insert your account password here>") 
 keep_sign_in = driver.find_element_by_class_name('css-12k6l22').click()  #if exists 
 sign_in = driver.find_element_by_css_selector('.css-1i45fk4.edzik9p0').click()
 
 #type some random thing in the terminal and hit enter and enjoyed having your cookies saved for next time!
 foo = input()
-save_cookie(driver, 'C:/Users/new22/Downloads/selenium/tmp/cookie')
+save_cookie(driver, './tmp/cookie')
