b6112458cedaee487d5569d14f86eaa79bfdbe7f
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2021-01-09 14:34:03 -0800

    initial commit

---
+++ backup.py
@@ -0,0 +1,9 @@
+# #navigate to sign-in page
+# elem = driver.find_element_by_class_name("css-1xh2uh0").click()
+# #fill in the form, fill in your account info here
+# email = driver.find_element_by_id("username")
+# email.send_keys("new2282307660@gmail.com")  
+# password = driver.find_element_by_id("password")
+# password.send_keys("@$smolFitness!") 
+# keep_sign_in = driver.find_element_by_class_name('css-12k6l22').click()  #if exists 
+# sign_in = driver.find_element_by_css_selector('.css-1i45fk4.edzik9p0').click()
